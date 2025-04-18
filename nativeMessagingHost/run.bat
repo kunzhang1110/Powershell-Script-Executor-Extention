73838f8f2b7411f2303341cd4bdf5b6950f0eeeb
Kun Zhang <kunzhang1110@gmail.com>
2025-04-19 01:59:48 +1000

    chore: modify gitignore

--- nativeMessagingHost/run.bat
+++
@@ -1,5 +0,0 @@
-@REM echo off is crucial to avoid disconnecting the port
-@echo off 
-@REM powershell -ExecutionPolicy Bypass -File "C:\\Users\\KZhang\\Documents\\GitHub\\Script-Scheduler\\Chime.ps1" > nul
-call python "C:\\Users\\KZhang\\Documents\\GitHub\\Utilities\\Powershell-Script-Executor-Extention\\nativeMessagingHost\\nativeMessagingHost.py" 
-pause
